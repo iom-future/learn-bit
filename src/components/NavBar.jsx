57dcd7e7b2b1562626667d7ebda00811ed69792d
iom_future <isaacmoses11111@gmail.com>
2026-01-01 06:36:20 -1100

    feat: add NavBar component and update dependencies

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome' //components
+import { faUser, faHeart, faHome } from '@fortawesome/free-solid-svg-icons' //icons
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { useRef } from 'react'
+function NavBar() {
+  return (
+    <nav>
+        
+    </nav>
+  )
+}
+
+export default NavBar
